b9e5252734e84268e2ce42f0610a74ee52d781ab
SeifRashedd <seifrashedswe@gmail.com>
2024-05-16 12:48:30 +0300

    main

--- lib/layouts/dashbord_desktop_layout.dart
+++ lib/layouts/dashbord_desktop_layout.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:responsive_credit_card_app/widgets/for_all/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_credit_card_app/widgets/drawer_section/custom_drawer.dart';
-import 'package:responsive_credit_card_app/widgets/for_all/my_card_and_transaction_history.dart';
+
 import 'package:responsive_credit_card_app/widgets/income_section/income_section.dart';
 
 class DashBordDesktopLayout extends StatelessWidget {
