862ab0cad80d9f6e1160818897c51d6ecc652dc2
SeifRashedd <seifrashedswe@gmail.com>
2024-05-18 15:50:00 +0300

    finish mobile layout

--- lib/views/dashbord_view.dart
+++ lib/views/dashbord_view.dart
@@ -3,14 +3,38 @@ import 'package:responsive_credit_card_app/layouts/adabtive_layout_widget.dart';
 import 'package:responsive_credit_card_app/layouts/dashbord_desktop_layout.dart';
 import 'package:responsive_credit_card_app/layouts/dashbord_mobile_layout.dart';
 import 'package:responsive_credit_card_app/layouts/dashbord_tablet_layout.dart';
+import 'package:responsive_credit_card_app/widgets/drawer_section/custom_drawer.dart';
 
-class DashBordView extends StatelessWidget {
+class DashBordView extends StatefulWidget {
   const DashBordView({super.key});
 
   @override
+  State<DashBordView> createState() => _DashBordViewState();
+}
+
+class _DashBordViewState extends State<DashBordView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < 800
+          ? AppBar(
+              elevation: 0,
+              backgroundColor: const Color(0xFF4DB7F2),
+              leading: IconButton(
+                onPressed: () {
+                  scaffoldKey.currentState!.openDrawer();
+                },
+                icon: const Icon(
+                  Icons.menu,
+                ),
+              ),
+            )
+          : null,
       backgroundColor: const Color(0xffF7F9FA),
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
       body: AdabtiveLayout(
         mobileLayout: (context) => const DashBordMobileLayout(),
         tabletLayout: (context) => const DashBordTabletLayout(),
